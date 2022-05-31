6a36c72fd14b6588f6694ecc42a29292e0783584
Vinay Hegde <76780478+vinayhegde105@users.noreply.github.com>
2022-05-31 21:33:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "vinay.hegde@insight.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "vinay.hegde@insight.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }

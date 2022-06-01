90071c78f1f19d1b1d89b2372d51ad79fc514fcb
Vinay Hegde <76780478+vinayhegde105@users.noreply.github.com>
2022-06-01 11:30:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+  
+module "sandbox1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "vinay.hegde@insight.com"
+    AccountName               = "INFRASTRUCTURE SHARING"
+    ManagedOrganizationalUnit = "Workload"
+    SSOUserEmail              = "vinay.hegde@insight.com"
+    SSOUserFirstName          = "Workload"
+    SSOUserLastName           = "Sharing"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "non-prod"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
